a2e5513e4c99afbd840a3214565ca471bff7260f
jt.boron <JT.BORON@GMAIL.COM>
2022-11-20 12:11:37 -0500

    added delays and state monitor now it works both audios play fully and change on state change

--- sketch_nov19b.ino
+++ sketch_nov19b.ino
@@ -4,8 +4,9 @@
 #include "SPI.h"
 
 TMRpcm tmrpcm;
+int laststate = -1;
 
-
+//==================================================
 void setup() {
 pinMode(7, INPUT_PULLUP);
 tmrpcm.speakerPin=9;
@@ -15,28 +16,54 @@ if(!SD.begin(SD_ChipSelectPin))
   Serial.println("SD fail");
   return;
 }
-tmrpcm.setVolume(5);
-
+tmrpcm.setVolume(6);
 
 }
 
+//==================================================
 void loop() {
  int bState = digitalRead(7);
 
+
+ if(bState != laststate){
+
     Serial.println(bState);
+    laststate = bState;
     if (bState == HIGH) {
 
-        Serial.println("pin high");
-            tmrpcm.play("start.wav");
-
-
-  } else {
-
-      Serial.println("pin low");
+   
+            Serial.println("pin high");
+            stopActions();
   
 
+  } else {
+    
+  
+       startActions();
+      Serial.println("pin low");
+
+  
+  }
 
 
   }
+
+}
+
+//==================================================
+int stopActions(){
+
+ Serial.println("stop actions");
+   tmrpcm.play("finished.wav");
+      delay(3000); 
+
+}
+
+//==================================================
+int startActions(){
+
+ Serial.println("startactions");
+  tmrpcm.play("start.wav");
+     delay(16000); 
 
 }
